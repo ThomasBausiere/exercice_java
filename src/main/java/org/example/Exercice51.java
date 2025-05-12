901ab8f6bed775eb4a61a68f178a27b944e22810
Thomas <t.bausiere@gmail.com>
2025-05-12 14:44:46 +0200

    first commit

---
+++ src/main/java/org/example/Exercice51.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Exercice51 {
+    public static void main(String[] args) {
+        /*
+        *
+        *
+        *
+        *
+        */
+
+        String nom, prenom;
+        nom= "toto";
+        prenom="tata";
+        System.out.println("Bonjour "+prenom+" "+nom);
+
+    }
+}
