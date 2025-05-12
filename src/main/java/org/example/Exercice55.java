b985cec7b5bf3ee1950bcc6128795e1f7066dc80
Thomas <t.bausiere@gmail.com>
2025-05-12 16:11:52 +0200

    exercice Java n°55

---
+++ src/main/java/org/example/Exercice55.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Exercice55 {
+    public static void main(String[] args) {
+/*
+    1. Créer un programme qui permet de calculer le volume d'un cône
+    2. La formule est la suivante : 1/3 × π × r² × h
+*/
+        System.out.println("Début du programme");
+        Scanner scanner = new Scanner(System.in);
+        double rayon, hauteur, result;
+        System.out.println("Calcul du volume d'un cône.");
+        System.out.println("Veuillez saisir le rayon");
+        rayon = scanner.nextDouble();
+        System.out.println("Veuillez saisir la hauteur");
+        hauteur = scanner.nextDouble();
+        System.out.println("Calcul en cours. . .");
+        result =(Math.PI*(Math.pow(rayon, 2))*hauteur)/3;
+
+
+        System.out.printf("Le volume du cône saisi est de: %, .2f cm3",result);
+
+    }
+}
