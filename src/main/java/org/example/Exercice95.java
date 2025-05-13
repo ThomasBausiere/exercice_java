963acd7714a84c4293f53d1338670890b6244974
Thomas <t.bausiere@gmail.com>
2025-05-13 09:44:19 +0200

    Exercice Java n°95

---
+++ src/main/java/org/example/Exercice95.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercice95 {
+    public static void main(String[] args) {
+/*
+        1. Déclarer deux tableaux de 10 éléments
+        2. Le premier tableau contiendra des nombres négatifs et positifs
+        3. Ajouter tous les éléments positifs du premier tableau dans le
+           second tableau
+*/
+
+        System.out.println("Début du programme");
+
+        int[] tableauSource= {-5, 3, 24, -12, -10, 5, 100, -2, -71, 0};
+        int[] tableauDestination={3, 24, 5, 100, 0, 0, 0, 0, 0, 0};
+
+        //on affiche nos tableaux pour une meilleur lisibilité
+        System.out.println(Arrays.toString(tableauSource));
+        System.out.println(Arrays.toString(tableauDestination));
+
+        for (int i=0; i<tableauSource.length; i++){
+            if(tableauSource[i] >=0){
+                tableauDestination[i]=tableauSource[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(tableauSource));
+        System.out.println(Arrays.toString(tableauDestination));
+        System.out.println("Fin du programme");
+
+
+    }
+}
