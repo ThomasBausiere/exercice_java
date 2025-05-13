8f29ad0124d668620aa916a9f8007e7a68c220cf
Thomas <t.bausiere@gmail.com>
2025-05-13 10:00:29 +0200

    Exercice Java n°96

---
+++ src/main/java/org/example/Exercice96.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Exercice96 {
+    public static void main(String[] args) {
+/*
+    Ecrire un programme qui permet de trouver la valeur maximale
+    dans un tableau
+
+ */
+
+        System.out.println("Début du programme");
+        System.out.println("Programme permettant de trouver la valeur maximale d'un tableau");
+
+        int[] tableau ={23, 150, 12, 28, 59, 2};
+        System.out.println(Arrays.toString(tableau));
+        int max=0;
+        for( int i=0;i<tableau.length; i++){
+            boolean isEmpty = (i==0) ? (isEmpty=true): (isEmpty= false);
+            if(isEmpty){
+                max=tableau[i];
+            } else if (max < tableau[i]) {
+                max = max=tableau[i];
+            }
+        }
+        System.out.println(max);
+
+        System.out.println("Fin du programme.");
+    }
+}
