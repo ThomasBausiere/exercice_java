6edd61e89cceca9171813b449f137a64552a3c14
Thomas <t.bausiere@gmail.com>
2025-06-27 13:35:27 +0200

    adding exo

--- tdd_bdd/exo_Junit/src/test/java/org.example/GradingCalculatorTest.java
+++ tdd_bdd/exo_Junit/src/test/java/org.example/GradingCalculatorTest.java
@@ -8,22 +8,44 @@ public class GradingCalculatorTest {
         private GradingCalculator gradingCalculator;
 
     @Test
-    public void whenGetGrade_90_70_Then_A_OrWhenGetGrade_80_60_Then_B_OrWhenGetGrade_60_60_Then_C_OrWhenElse_Then_F(){
+    public void whenGetGrade_90_70_Then_A(){
         //ARRANGE
-        int a = 60;
+        int a = 00;
         int b = 70;
         String result  = "";
         //ACT
         if(a > 90 && b > 70){
             result = "A";
-        }else if(a > 80 && b > 60){
-            result = "B";
-        } else if (a > 60 && b > 60) {
-            result = "C";
-        }else{
-            result = "F";
         }
         //ASSERT
-        Assert.assertEquals("F", result);
+        Assert.assertEquals("A", result);
+    }
+
+    @Test
+    public void whenGetGrade_80_60_Then_B(){
+        int a = 00;
+        int b = 70;
+        String result  = "";
+        if(a > 80 && b > 60){
+            result = "B";
+        }
+    }
+    @Test
+    public void whenGetGrade_60_60_Then_C(){
+        int a = 00;
+        int b = 70;
+        String result  = "";
+        if (a > 60 && b > 60) {
+            result = "C";
+        }
+
+    @Test
+    public void whenElse_Then_F(){
+                int a = 00;
+                int b = 70;
+                String result  = "";
+            if (a <= 60 && b <= 60) {
+                result = "F"
+            }
     }
 }
